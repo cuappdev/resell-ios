72d319b1bd786bfdbe5ff117e062f8eee4d34192
Richie Sun <rs929@cornell.edu>
2024-10-18 03:27:08 -0400

    Finished screen

--- Resell/Views/ProductDetailsView.swift
+++ Resell/Views/ProductDetailsView.swift
@@ -40,8 +40,21 @@ struct ProductDetailsView: View {
             
             DraggableSheetView(maxDrag: viewModel.maxDrag) {
                 detailsView
+                    .ignoresSafeArea()
             }
             .ignoresSafeArea()
+
+            buttonGradientView
+
+            if viewModel.didShowOptionsMenu {
+                OptionsMenuView(showMenu: $viewModel.didShowOptionsMenu, options: [
+                    // TODO: Replace with Deeplink
+                    .share(url: URL(string: "https://www.google.com")!, itemName: item.title),
+                    .report(destination: AnyView(Text("Placeholder"))),
+                    .delete
+                ])
+                .zIndex(1)
+            }
         }
         .ignoresSafeArea()
         .toolbar {
@@ -104,36 +117,26 @@ struct ProductDetailsView: View {
 
     private var detailsView: some View {
         GeometryReader { geometry in
-            VStack {
-                RoundedRectangle(cornerRadius: 4)
-                    .frame(width: 50, height: 8)
-                    .foregroundStyle(Constants.Colors.inactiveGray)
-                    .padding(.top, 12)
+            VStack(alignment: .leading) {
                 HStack {
-                    Text(item.title)
-                        .font(Constants.Fonts.h2)
-                        .foregroundStyle(Constants.Colors.black)
-
-                    Spacer()
-
-                    Text("$\(item.price)")
-                        .font(Constants.Fonts.h2)
-                        .foregroundStyle(Constants.Colors.black)
+                    RoundedRectangle(cornerRadius: 4)
+                        .frame(width: 50, height: 8)
+                        .foregroundStyle(Constants.Colors.inactiveGray)
+                        .padding(.top, 12)
+                        .frame(alignment: .center)
                 }
+                .frame(maxWidth: .infinity, alignment: .center)
 
-                HStack {
-                    Image(seller.1)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 32, height: 32)
-                        .clipShape(Circle())
 
-                    Text(seller.0)
-                        .font(Constants.Fonts.body2)
-                        .foregroundStyle(Constants.Colors.black)
+                titlePriceView
 
-                    Spacer()
-                }
+                sellerProfileView
+                    .padding(.bottom, 24)
+
+                itemDescriptionView
+                    .padding(.bottom, 32)
+
+                similarItemsView
 
                 Spacer()
             }
@@ -142,6 +145,78 @@ struct ProductDetailsView: View {
             .cornerRadius(40)
             .position(x: UIScreen.width / 2, y: max(150, UIScreen.main.bounds.width * viewModel.maxImgRatio - 50) + geometry.size.height / 2)
         }
+    }
+
+    private var titlePriceView: some View {
+        HStack {
+            Text(item.title)
+                .font(Constants.Fonts.h2)
+                .foregroundStyle(Constants.Colors.black)
+
+            Spacer()
+
+            Text("$\(item.price)")
+                .font(Constants.Fonts.h2)
+                .foregroundStyle(Constants.Colors.black)
+        }
+    }
+
+    private var sellerProfileView: some View {
+        HStack {
+            Image(seller.1)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 32, height: 32)
+                .clipShape(Circle())
+
+            Text(seller.0)
+                .font(Constants.Fonts.body2)
+                .foregroundStyle(Constants.Colors.black)
+
+            Spacer()
+        }
+    }
+
+    private var itemDescriptionView: some View {
+        Text("Vintage blue pants that are super cool!\nCondition\nISBN\nAdditional Information")
+            .font(Constants.Fonts.body2)
+            .foregroundStyle(Constants.Colors.black)
+    }
+
+    private var similarItemsView: some View {
+        VStack(alignment: .leading) {
+            Text("Similar Items")
+                .font(Constants.Fonts.title1)
+                .foregroundStyle(Constants.Colors.black)
+
+            HStack {
+                let imageSize = (UIScreen.width - 72) / 4
+                // TODO: Replace with similar items logic
+                ForEach(0..<4, id: \.self) { _ in
+                    Image("justin_long")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: imageSize, height: imageSize)
+                        .clipShape(.rect(cornerRadius: 25))
+                }
+            }
+        }
+    }
+
+    private var buttonGradientView: some View {
+        VStack {
+            PurpleButton(text: "Contact Seller") {
+                // TODO: Chat with Seller
+            }
+        }
+        .frame(width: UIScreen.width, height: 50)
+        .background(
+            LinearGradient(stops: [
+                .init(color: Color.clear, location: 0.0),
+                .init(color: Constants.Colors.white.opacity(0.8), location: 0.5),
+                .init(color: Constants.Colors.white, location: 1.0)
+            ], startPoint: .top, endPoint: .bottom)
+        )
     }
 
 }
