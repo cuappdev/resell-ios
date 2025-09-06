eccb09a5ce9c3126a0de4a552c5f3fd4a606e1b9
Peter <mrpeterssyt@gmail.com>
2025-09-06 10:59:28 -0400

    Update Report.swift

--- Resell/Models/Report.swift
+++ Resell/Models/Report.swift
@@ -23,3 +23,40 @@ struct ReportMessageBody: Codable {
     let message: MessageBody
     let reason: String
 }
+
+struct Report: Codable, Identifiable {
+    let id: String
+    let reporter: User
+    let reported: User
+    let post: Post?
+    let message: ReportMessage?
+    let reason: String
+    let type: ReportType
+    let resolved: Bool
+    let created: Date
+
+    enum ReportType: String, Codable {
+        case post
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case reporter
+        case reported
+        case post
+        case message
+        case reason
+        case type
+        case resolved
+        case created
+    }
+}
+
+struct ReportMessage: Codable {
+    let id: String
+    let content: String
+    let sender: User
+    let receiver: User
+    let created: Date
+    let read: Bool
+}
