607a1c1528839946ff1cd457004b1f72c68d7d19
Charles <cll258@cornell.edu>
2025-09-26 12:47:32 -0400

    removed some prints

--- Resell/ViewModels/HomeViewModel.swift
+++ Resell/ViewModels/HomeViewModel.swift
@@ -87,7 +87,6 @@ class HomeViewModel: ObservableObject {
                 let postsResponse = try await NetworkManager.shared.getSavedPosts()
                 savedItems = Post.sortPostsByDate(postsResponse.posts)
                 completion()
-                print("SAVED ITEMS: \(savedItems)")
             } catch {
                 NetworkManager.shared.logger.error("Error in HomeViewModel.getSavedPosts: \(error)")
             }
@@ -98,11 +97,8 @@ class HomeViewModel: ObservableObject {
     func filterPosts() {
         Task {
             do {
-                print("Filtering by \(selectedFilter)")
                 let postsResponse = try await NetworkManager.shared.getFilteredPosts(by: [selectedFilter])
                 filteredItems = postsResponse.posts
-                print(filteredItems.count)
-                
             } catch {
                 NetworkManager.shared.logger.error("Error in HomeViewModel.filterPosts: \(error)")
             }
@@ -148,7 +144,6 @@ class HomeViewModel: ObservableObject {
             }
             
             group.notify(queue: .main) {
-                print("Does this ever get callled ??")
                 completion()
             }
     }
