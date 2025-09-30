1ca2ba74228463d71e8ca6ae96acb72f1ca4ff0f
Charles <cll258@cornell.edu>
2025-09-30 19:54:37 -0400

    hi

--- Resell/Views/Components/ProductsGalleryView.swift
+++ Resell/Views/Components/ProductsGalleryView.swift
@@ -35,13 +35,19 @@ struct ProductsGalleryView: View {
         ScrollView(.vertical, showsIndicators: true) {
             HStack(alignment: .top, spacing: 20) {
                 LazyVStack(spacing: 20) {
-                    ForEach(column1) { post in
+                    ForEach(column1, id: \.id) { post in
                         ProductGalleryCell(selectedItem: $selectedItem, post: post, savedCell: false)
+//                            .onAppear {
+//                                // pls fix me im dying 
+//                                if post == column1.last  {
+//                                    onScrollToBottom?()
+//                                }
+//                            }
                     }
                 }
 
                 LazyVStack(spacing: 20) {
-                    ForEach(column2) { post in
+                    ForEach(column2, id: \.id) { post in
                         ProductGalleryCell(selectedItem: $selectedItem, post: post, savedCell: false)
                     }
                 }
